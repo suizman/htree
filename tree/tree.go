d07ef92743ab1d9519b8378db66a82a048cdc2f7
Martin Evgeniev <suizman@outlook.com>
2018-07-16 18:06:33 +0200

    WIP Travel into nodes.

--- tree/tree.go
+++ tree/tree.go
@@ -2,6 +2,8 @@ package tree
 
 import (
 	"encoding/binary"
+	"fmt"
+	"math"
 
 	"github.com/bbva/qed/hashing"
 )
@@ -11,6 +13,7 @@ type Tree struct {
 	version uint64
 	store   Node
 }
+
 type Node struct {
 	hashoff map[Pos]Digest
 }
@@ -47,6 +50,43 @@ func NewTree(id string, version uint64, store Node) *Tree {
 
 	return &Tree{[]byte(id), version, store}
 
+}
+
+func Travel(i uint64, p Pos) bool {
+
+	foreverAlone := Pos{
+		index: 0,
+		layer: 0,
+	}
+
+	if p == foreverAlone {
+		fmt.Printf("First node: %v\n", p)
+		return true
+	}
+
+	if i <= p.layer {
+		fmt.Printf("Go left. Index: %v|Layer: %v\n", p.index, p.layer)
+		Travel(i, Pos{
+			index: p.index,
+			layer: p.layer - 1,
+		})
+	} else {
+		fmt.Printf("Go Right. Index: %v|Layer: %v\n", p.index, p.layer)
+		Travel(i, Pos{
+			index: p.index + pow(2, p.layer) - 1,
+			layer: p.layer - 1,
+		})
+	}
+	return true
+}
+
+func getDepth(index uint64) uint64 {
+	return uint64(math.Ceil(math.Log2(float64(index + 1))))
+}
+
+// Utility to calculate arbitraty pow and return an int64
+func pow(x, y uint64) uint64 {
+	return uint64(math.Pow(float64(x), float64(y)))
 }
 
 // uInt64AsBytes returns the []byte representation of a unit64
